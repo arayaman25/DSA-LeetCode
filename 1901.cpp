0fc5267fa7ed6878a70190bb0f480b1887ed5f04
arayaman25 <arayamanios9@gmail.com>
2026-01-07 16:36:19 +0530

    Update

---
+++ 1901.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+    int MaxInRowIndex(vector<vector<int>>& nums,int n,int m,int col){
+        int maxi=-1;
+        int index=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i][col] > maxi){
+                maxi=nums[i][col];
+                index = i;
+            }
+        }
+        return index;
+
+    }
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int low=0,high=m-1;
+        while(low<=high){
+            int mid=low + (high-low)/2;
+            int row=MaxInRowIndex(mat,n,m,mid);
+            int left =-1;
+            int right =-1;
+
+            if(mid-1 >= 0)  left=mat[row][mid-1];
+            if(mid+1 < m)   right=mat[row][mid+1];
+           
+            if(mat[row][mid] > left && mat[row][mid] > right) return {row,mid};
+            else if (mat[row][mid] < left) high=mid-1;
+            else low=mid+1;
+
+
+        }
+        return {-1,-1};
+    }
--- 240.cpp
+++ 240.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+// Brute n X log m
     bool LowerBound(vector<int>& arr,int n,int x){
     int low=0,high=n-1;
     int ans=n;
@@ -14,12 +15,26 @@ using namespace std;
     if (ans == n) return false;             
     return arr[ans] == x;
   }
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    bool Brute_searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
         for(int i=0;i<n;i++){
             if(LowerBound(matrix[i],m,target)) return true;
       }
       return false;
+      
+    }
+// Optimal m+n
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int row=0,col=m-1;
+        while(row<n && col>=0){
+            int val=matrix[row][col];
+            if(val==target) return true;
+            else if(val>target) col--;
+            else row++;
+        }
+        return false;
         
     }
