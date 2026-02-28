c7ae748c778bcd96e19360e70bca65802959fdbb
arayaman25 <arayamanios9@gmail.com>
2026-02-28 21:35:33 +0530

    Initial commit

---
+++ 25.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* findKth(ListNode* head, int k) {
+        ListNode* temp=head;
+        ListNode* NodeK=NULL;
+        while(temp!=NULL){
+            k--;
+            if(k==0){
+                NodeK=temp;
+                break;
+            }
+            temp=temp->next;
+        }
+        return NodeK;
+    }
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* newHead=reverseList(head->next);
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=NULL;
+        return newHead;   
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp=head;
+        ListNode* nextNode=NULL;
+        ListNode* prevNode=NULL;
+        while(temp!=NULL){
+            ListNode* KthNode=findKth(temp,k);
+            if(KthNode==NULL){
+                if(prevNode) prevNode->next=temp;
+                break;
+            }
+            nextNode=KthNode->next;
+            KthNode->next=NULL;
+            reverseList(temp);
+            if(temp==head){
+                head=KthNode;
+            }
+            else{
+                prevNode->next=KthNode;
+            }
+            prevNode=temp;
+            temp=nextNode;
+        }
+        return head;
+    }
+};
---
+++ removeDupliFromDLL.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode *prev;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1, ListNode *next1, ListNode *prev1)
+    {
+        val = data1;
+        next = next1;
+        prev = prev1;
+    }
+};
+
+
+class Solution {
+public:
+    ListNode * removeDuplicates(ListNode *head) {
+        ListNode* temp=head;
+        while(temp!= NULL && temp->next!=NULL){
+            ListNode* nextNode=temp->next;
+            while(nextNode!=NULL && nextNode->val==temp->val){
+                ListNode* duplicate=nextNode;
+                nextNode=nextNode->next;
+                delete duplicate;
+            }
+            temp->next=nextNode;
+            if(nextNode) nextNode->prev=temp;
+            temp=temp->next;
+        }
+        return head;
+    }
+};
