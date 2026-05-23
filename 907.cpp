c1b0685ba625405e4c5e7d7adecbcaf8fad2bca8
arayaman25 <arayamanios9@gmail.com>
2026-05-23 13:32:15 +0530

    new

--- 907.cpp
+++ 907.cpp
@@ -27,3 +27,104 @@ public:
         return sum;
     }
 };
+
+//optimal
+class Solution {
+public:
+    vector<int> nse(vector<int> nums){
+        int n=nums.size();
+        vector<int> nse(n);
+        stack<int> st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]>=nums[i]) st.pop();
+
+            if(st.empty()) nse[i]=n;
+            else nse[i]=st.top();
+
+            st.push(i);
+        }
+        return nse;
+    }
+    vector<int> pse(vector<int> nums){
+        int n=nums.size();
+        vector<int> pse(n);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]>nums[i]) st.pop();
+
+            if(st.empty()) pse[i]=-1;
+            else pse[i]=st.top();
+            
+            st.push(i);
+        }
+        return pse;
+    }
+
+    long long sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> nsee=nse(arr);
+        vector<int> psee=pse(arr);
+        long long total = 0;
+        for(int i=0;i<n;i++){
+            long long left=i-psee[i];
+            long long right=nsee[i]-i;
+            total = (total + (right * left * 1LL * arr[i]));
+        }
+        return total;
+
+    }
+
+
+    vector<int> nge(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> nge(n);
+        stack<int> st;
+        for(int i =n-1; i >= 0; i--) {
+            while(!st.empty() && nums[st.top()] <= nums[i]) {
+                st.pop();
+            }
+
+            if(st.empty()) nge[i] = n;
+            else nge[i] = st.top();
+
+            st.push(i);
+        }
+        return nge;
+    }
+
+    vector<int> pge(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> nge(n);
+        stack<int> st;
+        for(int i = 0; i <n; i++) {
+            while(!st.empty() && nums[st.top()] < nums[i]) {
+                st.pop();
+            }
+
+            if(st.empty()) nge[i] = -1;
+            else nge[i] = st.top();
+
+            st.push(i);
+        }
+        return nge;
+    }
+
+    long long sumSubarrayMaxs(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> ngee=nge(arr);
+        vector<int> pgee=pge(arr);
+        long long total = 0;
+        for(int i=0;i<n;i++){
+            long long left=i-pgee[i];
+            long long right=ngee[i]-i;
+            total = (total + (right * left * 1LL * arr[i]));
+        }
+        return total;
+
+    }
+
+
+    long long subArrayRanges(vector<int>& nums) {
+        return sumSubarrayMaxs(nums)-sumSubarrayMins(nums);
+    }
+};
