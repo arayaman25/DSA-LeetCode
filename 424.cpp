80198ed75e9baeffb879cba3a75232208fd72950
arayaman25 <arayamanios9@gmail.com>
2026-06-26 09:19:54 +0530

    updated

---
+++ 424.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+//O(n^2)
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.size();
+        int maxi=0;
+        
+        for(int i=0;i<n;i++){
+            vector<int> freq(26,0);
+            int maxf=0;
+            for(int j=i;j<n;j++){
+                freq[s[j]-'A']++;
+                maxf=max(maxf,freq[s[j]-'A']);
+                int changes=(j-i+1)-maxf;
+                if(changes<=k) maxi=max(maxi,j-i+1);
+                else break;
+            }
+        }
+        return maxi;
+    }
+};
+
+//O(N)
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.size();
+        int maxi=0;
+        int l=0,r=0;
+        vector<int> freq(26,0);
+        int maxf=0;
+        for(r=0;r<n;r++){
+            freq[s[r]-'A']++;
+            maxf=max(maxf,freq[s[r]-'A']);
+            int changes=(r-l+1)-maxf;
+            if(changes>k){
+                freq[s[l]-'A']--;
+                l++;
+            }
+            maxi=max(maxi,r-l+1);
+
+        }
+        return maxi;
+    }
+};
---
+++ 904.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+//O(N+N)
+class Solution {
+public:
+
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        unordered_map<int,int> mp;
+        int left = 0;
+        int maxi = 0;
+        for(int right = 0; right < n; right++) {
+            mp[fruits[right]]++;
+            while(mp.size() > 2) {
+                mp[fruits[left]]--;
+                if(mp[fruits[left]] == 0) mp.erase(fruits[left]);
+                left++;
+            }
+            maxi=max(maxi,right-left+1);
+        }
+        return maxi;
+    }
+};
+
+//O(N)
+class Solution {
+public:
+
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        unordered_map<int,int> mp;
+        int left = 0;
+        int maxi = 0;
+        for(int right = 0; right < n; right++) {
+            mp[fruits[right]]++;
+            if(mp.size() > 2) {
+                mp[fruits[left]]--;
+                if(mp[fruits[left]] == 0) mp.erase(fruits[left]);
+                left++;
+            }
+            else maxi=max(maxi,right-left+1);
+        }
+        return maxi;
+    }
+};
