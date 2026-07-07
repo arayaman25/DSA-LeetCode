56eb68dc6acb74c0be9db17841b46f31700067da
arayaman25 <arayamanios9@gmail.com>
2026-07-07 23:44:14 +0530

    nvm

---
+++ 1918.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+23class Solution {
+public:
+    int kthSmallest(vector<int> &arr, int k) {
+
+        int n = arr.size();
+
+        priority_queue<int> pq;
+
+        for(int i = 0; i < n; i++) {
+
+            int sum = 0;
+
+            for(int j = i; j < n; j++) {
+
+                sum += arr[j];
+
+                if(pq.size() < k) {
+                    pq.push(sum);
+                }
+                else if(sum < pq.top()) {
+                    pq.pop();
+                    pq.push(sum);
+                }
+            }
+        }
+
+        return pq.top();
+    }
+};
