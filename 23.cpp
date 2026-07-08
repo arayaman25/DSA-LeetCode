8974782540fa3daed912c7d70161bd0a459478b0
arayaman25 <arayamanios9@gmail.com>
2026-07-08 12:08:31 +0530

    nvm

---
+++ 23.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+//Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+
+class compare {
+    public:
+    bool operator()(ListNode* l1, ListNode* l2) {
+        return l1->val > l2->val;
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy; 
+        priority_queue < ListNode* , vector<ListNode*> , compare> minHeap;
+        
+        for(ListNode* head : lists){
+            if(head) minHeap.push(head);
+        }
+
+        while(!minHeap.empty()) {
+            ListNode* curr=minHeap.top();
+            tail -> next = curr;
+            tail = tail -> next;
+            minHeap.pop();
+
+            if(curr->next) minHeap.push(curr -> next);
+        }
+
+        return dummy->next;
+        
+    }
+};
