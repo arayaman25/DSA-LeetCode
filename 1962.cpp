afcdd2a81cbe57d101e7bb4ddc2bc91c4ff3b7d0
arayaman25 <arayamanios9@gmail.com>
2026-07-11 12:27:43 +0530

    lets go

---
+++ 1962.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int> maxHeap;
+        for( int &x : piles ){
+            maxHeap.push(x);
+        }
+
+        while(k) {
+            int top = maxHeap.top();
+            int x = top - (int)floor(top / 2) ;
+            maxHeap.pop();
+            maxHeap.push(x);
+            k--;
+        }
+
+        int sum=0;
+        while(maxHeap.size()){
+            sum += maxHeap.top();
+            maxHeap.pop();
+        }
+        return sum;
+    }
+};
---
+++ 355.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<map>
+#include<set>
+#include<unordered_set>
+using namespace std;
+
+class Twitter {
+
+private:
+    unordered_map<int, vector<int>> userTweetsMap;
+    unordered_map<int, unordered_set<int>> userFollowingMap;
+    unordered_map<int, int> tweetTimestampMap;
+    int timestamp;
+
+public:
+    Twitter() {
+        timestamp=0;
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        userTweetsMap[userId].push_back(tweetId);
+        tweetTimestampMap[tweetId] = ++timestamp;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        unordered_set<int> followingSet;
+        if (userFollowingMap.find(userId) != userFollowingMap.end()) {
+            followingSet = userFollowingMap[userId];
+        }
+
+        unordered_set<int> allRelevantUsers = followingSet;
+        allRelevantUsers.insert(userId);
+
+        priority_queue<pair<int, int>> tweetQueue;
+        for (int currentUserId : allRelevantUsers) {
+            if (userTweetsMap.find(currentUserId) != userTweetsMap.end()) {
+                vector<int>& currentUserTweets = userTweetsMap[currentUserId];
+              
+                if (!currentUserTweets.empty()) {
+                    int tweetsToAdd = min(10, static_cast<int>(currentUserTweets.size()));
+                    for (int i = currentUserTweets.size() - 1; 
+                         i >= static_cast<int>(currentUserTweets.size()) - tweetsToAdd; 
+                         i--) {
+                        int tweetId = currentUserTweets[i];
+                        tweetQueue.push({tweetTimestampMap[tweetId], tweetId});
+                    }
+                }
+            }
+        }
+
+        vector<int> newsFeed;
+        while (!tweetQueue.empty() && newsFeed.size() < 10) {
+            newsFeed.push_back(tweetQueue.top().second);
+            tweetQueue.pop();
+        }
+      
+        return newsFeed;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        userFollowingMap[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        if (userFollowingMap.find(followerId) != userFollowingMap.end()) {
+            userFollowingMap[followerId].erase(followeeId);
+        }
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
