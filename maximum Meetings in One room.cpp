53d9807cadb47a2b815d63b2ad4da4885bee2ec4
arayaman25 <arayamanios9@gmail.com>
2026-07-15 16:57:17 +0530

    Max Meet - Greedy

---
+++ maximum Meetings in One room.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> maxMeetings(vector<int> &s, vector<int> &f) {
+        // code here
+        int n=s.size();
+        vector<int> ans;
+        vector<vector<int>> esi; //end , st ,ind
+
+        for (int i = 0; i < n; i++) {
+            esi.push_back({f[i], s[i], i});
+        }
+        sort(esi.begin(), esi.end(), [](vector<int> &a, vector<int> &b) {
+            if (a[0] == b[0])
+                return a[2] < b[2];   // smaller meeting number first
+            return a[0] < b[0];       // smaller finish time first
+        });
+        
+        ans.push_back(esi[0][2] +1);
+        int k=0;
+        for(int i=1 ; i<n ; i++){
+            if (esi[i][1] > esi[k][0]) {
+                ans.push_back(esi[i][2] + 1);
+                k=i;
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+
+    }
+};
