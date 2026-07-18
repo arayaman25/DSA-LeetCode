e2a58439b9fa9e1820ed134b8b6d49e97a8cab2b
arayaman25 <arayamanios9@gmail.com>
2026-07-18 19:11:57 +0530

    Candy

--- 135 Candy.cpp
+++ 135 Candy.cpp
@@ -27,3 +27,39 @@ public:
         return sum;
     }
 };
+
+//TC : O(n) SC:O(1)
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int i = 1 ;
+        int sum = 1;
+        while(i < n){
+            if(ratings[i] == ratings[i-1]){
+                sum += 1;
+                i++;
+                continue;
+            }
+
+            int peak = 1;
+            while(i < n && ratings[i] > ratings[i-1]) {
+                peak++;
+                sum += peak;å
+                i++;
+            }
+
+            int down = 1;
+            while(i < n && ratings[i] < ratings[i-1]) {
+                sum += down;
+                down++;
+                i++;
+            }
+
+            if(down > peak) sum += down - peak;
+
+        }
+        return sum;
+        
+    }
+};
