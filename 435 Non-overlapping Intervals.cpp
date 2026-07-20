e6db4e37c1911b6be64d8ffcc98130e69e97915b
arayaman25 <arayamanios9@gmail.com>
2026-07-21 00:22:45 +0530

    intervals

---
+++ 435 Non-overlapping Intervals.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//TC: O(N log N) , SC : O(1)
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), [](vector<int> &a, vector<int> &b) {
+            return a[1] < b[1];
+        });
+
+        int lastEnd = intervals[0][1];
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] >= lastEnd) {
+                count++;
+                lastEnd = intervals[i][1];
+            }
+        }
+
+        return n - count ;
+
+    }
+};
