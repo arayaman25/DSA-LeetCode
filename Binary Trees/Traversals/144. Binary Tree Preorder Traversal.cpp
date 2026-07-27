df68d854d01e62b80250ee08d2a264813b418f82
arayaman25 <arayamanios9@gmail.com>
2026-07-27 08:16:50 +0530

    Just POTD

--- Binary Trees/Traversals/144. Binary Tree Preorder Traversal.cpp
+++ Binary Trees/Traversals/144. Binary Tree Preorder Traversal.cpp
@@ -30,3 +30,30 @@ public:
         
     }
 };
+
+//using stack - LIFO
+// TC : O(N) SC: O(N) ie ht of BT
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preOrder;
+        if(root == NULL) return preOrder;
+
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+
+            preOrder.push_back(root -> val);
+
+            if(root -> right) st.push(root -> right);
+            if(root -> left) st.push(root -> left);
+        }
+    
+
+        return preOrder;
+        
+    }
+};
