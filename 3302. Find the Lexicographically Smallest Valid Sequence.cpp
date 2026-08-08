e7b133d927f31aa529085aa72b31889473a6b40c
arayaman25 <arayamanios9@gmail.com>
2026-08-08 15:03:43 +0530

    POTD

---
+++ 3302. Find the Lexicographically Smallest Valid Sequence.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//TC: O(n) word 1
+//SC: O(m) word 2
+
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<int> last(m, -1);
+        int j = m - 1;
+        for(int i = n - 1  ; i >= 0 ; i--){
+            if(word1[i] == word2[j]) {
+                last[j] = i;
+                j--;
+                if(j < 0) break;
+            }
+            
+        }
+
+        bool change = false;
+        j = 0;
+        vector<int> ans;
+        for(int i = 0 ; i < n ; i++){
+            if (j == m) break;
+
+            if(word1[i] == word2[j]){
+                ans.push_back(i);
+                j++;
+            }
+            else if( !change && (j == m-1 || last[j+1] > i)){
+                change = true;
+                ans.push_back(i);
+                j++;
+            }
+        }
+
+        if(ans.size() == m) return ans;
+        return {};
+
+    }
+};
