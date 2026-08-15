55d19c07ff5efe20dd4c12e7fc5f903c1a3768b6
arayaman25 <arayamanios9@gmail.com>
2026-08-15 11:50:25 +0530

    POTD

---
+++ 3090. Maximum Length Substring With Two Occurrences.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        vector<int> freq(26, 0);
+
+        int left = 0;
+        int maxi = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            freq[s[right] - 'a']++;
+
+            while (freq[s[right] - 'a'] > 2) {
+                freq[s[left] - 'a']--;
+                left++;
+            }
+
+            maxi = max(maxi, right - left + 1);
+        }
+
+        return maxi;
+    }
+};
