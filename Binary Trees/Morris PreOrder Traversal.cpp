747c463d33ce6333bd298a7c03cff9024d3c2292
arayaman25 <arayamanios9@gmail.com>
2026-08-22 12:32:12 +0530

    Morris

---
+++ Binary Trees/Morris PreOrder Traversal.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+
+  //Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+  
+
+
+//Time: O(n)
+//Space: O(1)
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        TreeNode* curr = root;
+
+        while(curr != NULL){
+            if(curr -> left == NULL){
+                result.push_back(curr -> val);
+                curr = curr -> right;
+            }
+            else{
+                TreeNode* leftChild = curr -> left;
+                while(leftChild -> right != NULL && leftChild -> right != curr){
+                    leftChild = leftChild -> right;
+                }
+
+                if (leftChild->right == NULL) {
+                    result.push_back(curr->val);
+                    leftChild->right = curr;
+                    curr = curr->left;
+                }
+                else {
+                    leftChild->right = NULL;
+                    curr = curr->right;
+                }
+            }
+            
+        }
+        return result;
+    }
+};
