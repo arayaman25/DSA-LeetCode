d5430c4eca87fd3b1ea9842db777c35432f754dc
arayaman25 <arayamanios9@gmail.com>
2026-09-21 10:00:35 +0530

    Gods Plan

--- Graphs/Directed Graph Cycle.cpp
+++ Graphs/Directed Graph Cycle.cpp
@@ -54,3 +54,65 @@ public:
         return false;
     }
 };
+
+//BFS - usinf KAHNS ALGO
+//TC:O(V + E)
+//SC:O(V + E)
+
+class Solution {
+public:
+    bool isCyclic(int V, vector<vector<int>>& edges) {
+
+        vector<vector<int>> adj(V);
+
+        // Build adjacency list
+        for(auto &edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+        }
+
+        // Calculate indegree
+        vector<int> inDeg(V, 0);
+
+        for(int u = 0; u < V; u++) {
+            for(int v : adj[u]) {
+                inDeg[v]++;
+            }
+        }
+
+        // Add all nodes with indegree 0
+        queue<int> que;
+
+        for(int i = 0; i < V; i++) {
+            if(inDeg[i] == 0) {
+                que.push(i);
+            }
+        }
+
+        // Kahn's Algorithm
+        int count = 0;
+
+        while(!que.empty()) {
+
+            int u = que.front();
+            que.pop();
+
+            count++;
+
+            for(int v : adj[u]) {
+
+                inDeg[v]--;
+
+                if(inDeg[v] == 0) {
+                    que.push(v);
+                }
+            }
+        }
+
+        // If not all vertices were processed, cycle exists
+        if(count == V) return false; 
+        return true;
+    }
+};
