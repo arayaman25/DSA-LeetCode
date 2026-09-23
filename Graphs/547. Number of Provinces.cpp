db7b14ff320ea1c2009c7c426c30e744920fedbd
arayaman25 <arayamanios9@gmail.com>
2026-09-23 13:30:10 +0530

    S

--- Graphs/547. Number of Provinces.cpp
+++ Graphs/547. Number of Provinces.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//DFS APPROACH
+
 class Solution {
 public:
     int n;
@@ -26,5 +28,47 @@ public:
             }
         }
         return count;
+    }
+};
+
+
+//BFS APPROACH
+class Solution {
+public:
+
+    void bfs(vector<vector<int>>& isConnected, vector<int>& vis, int start) {
+        int n = isConnected.size();
+
+        queue<int> q;
+        q.push(start);
+        vis[start] = 1;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (int v = 0; v < n; v++) {
+                if (isConnected[node][v] == 1 && !vis[v]) {
+                    vis[v] = 1;
+                    q.push(v);
+                }
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> vis(n, 0);
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                cnt++;
+                bfs(isConnected, vis, i);
+            }
+        }
+
+        return cnt;
     }
 };
